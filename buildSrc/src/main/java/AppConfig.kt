421bf2d279f0a3e7693c94a709e438329a931d4c
Jitty Andiyan <jittya@gmail.com>
2021-04-26 22:01:33 +0400

    App Name added in package name android

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -1,12 +1,12 @@
-import java.util.*
-
 object AppConfig {
     const val AppName = "KMMT App"
     const val versionCode = 1
     const val version = "0.1.1.21.4.26" //Major.Minor.intermediate.Year.Month.Day
     const val group = "com.jittyandiyan.mobile"
+
     object Android {
-        const val packageName= "$group.android"
+        var appName = AppName.replace(" ", "")
+        var packageName = "$group.$appName.android"
         const val compileSdkVersion = 29
         const val minSdkVersion = 21
         const val targetSdkVersion = 29
