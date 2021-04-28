f4110ea11dc310ff4e473f6cb113e83e6d962eed
Jitty Andiyan <jittya@gmail.com>
2021-04-28 11:41:11 +0400

    API Simplified

--- shared/src/commonMain/kotlin/com/jittyandiyan/shared/apis/JsonPlaceHolderServiceAPI.kt
+++ shared/src/commonMain/kotlin/com/jittyandiyan/shared/apis/JsonPlaceHolderServiceAPI.kt
@@ -1,23 +1,20 @@
 package com.jittyandiyan.shared.apis
 
 import com.jittyandiyan.shared.core.network.BaseAPI
-import com.jittyandiyan.shared.core.network.HTTPHelper
 import com.jittyandiyan.shared.models.PostModel
 
-class JsonPlaceHolderServiceAPI:BaseAPI() {
+class JsonPlaceHolderServiceAPI : BaseAPI() {
     override val baseUrl: String
         get() = "https://jsonplaceholder.typicode.com/"
 
-    suspend fun getPosts(postId:Int):List<PostModel>
-    {
-        return HTTPHelper().doGet {
+    suspend fun getPosts(postId: Int): List<PostModel> {
+        return doGet {
             apiPath("comments?postId=$postId")
         }
     }
 
-    suspend fun setPost(post:PostModel):PostModel
-    {
-        return HTTPHelper().doPost(post) {
+    suspend fun setPost(post: PostModel): PostModel {
+        return doPost(post) {
             apiPath("comments")
         }
     }
--- shared/src/commonMain/kotlin/com/jittyandiyan/shared/core/network/BaseAPI.kt
+++ shared/src/commonMain/kotlin/com/jittyandiyan/shared/core/network/BaseAPI.kt
@@ -1,12 +1,31 @@
 package com.jittyandiyan.shared.core.network
 
+import io.ktor.client.utils.*
 import io.ktor.http.*
 
 abstract class BaseAPI {
     abstract val baseUrl: String
+    val httpHelper = HTTPHelper()
 
     protected fun URLBuilder.apiPath(path: String) {
         takeFrom(baseUrl)
         encodedPath = path
+    }
+
+    suspend inline fun <reified T> doGet(
+        urlBuilder: URLBuilder.() -> Unit
+    ): T {
+        return httpHelper.doGet {
+            apply(urlBuilder)
+        }
+    }
+
+    suspend inline fun <reified T> doPost(
+        requestBody: Any = EmptyContent,
+        urlBuilder: URLBuilder.() -> Unit
+    ): T {
+        return httpHelper.doPost(requestBody) {
+            apply(urlBuilder)
+        }
     }
 }
