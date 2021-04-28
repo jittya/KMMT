d4a8bac5e7c1247ee8b78c64657000d2f0e8f20d
Jitty Andiyan <jittya@gmail.com>
2021-04-28 09:08:15 +0400

    Async optimized using flow instead of channelFlow

--- shared/src/commonMain/kotlin/com/jittyandiyan/shared/core/architecture/viewModel/async/Async.kt
+++ shared/src/commonMain/kotlin/com/jittyandiyan/shared/core/architecture/viewModel/async/Async.kt
@@ -4,8 +4,8 @@ import com.jittyandiyan.shared.core.expectations.ApplicationDispatcher
 import com.jittyandiyan.shared.core.expectations.Dispatchers_Default
 import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.channelFlow
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
 import kotlin.reflect.KSuspendFunction0
 import kotlin.reflect.KSuspendFunction1
 
@@ -27,18 +27,18 @@ abstract class Async {
         backgroundCoroutineScope.coroutineContext.cancelChildren(CancellationException(reason))
     }
 
-    fun <OUT> runOnBackground(function: () -> KSuspendFunction0<OUT>): Flow<OUT> = channelFlow {
-        var job = backgroundCoroutineScope.async<OUT> {
+    fun <OUT> runOnBackground(function: () -> KSuspendFunction0<OUT>): Flow<OUT> = flow {
+        val job = backgroundCoroutineScope.async {
             return@async function.invoke().invoke()
         }
-        send(job.await())
+        emit(job.await())
     }
 
-    fun <IN,OUT> runOnBackground(param: IN, function: () -> KSuspendFunction1<IN, OUT>): Flow<OUT> = channelFlow {
-        var job = backgroundCoroutineScope.async<OUT> {
+    fun <IN,OUT> runOnBackground(param: IN, function: () -> KSuspendFunction1<IN, OUT>): Flow<OUT> = flow {
+        val job = backgroundCoroutineScope.async {
             return@async function.invoke().invoke(param)
         }
-        send(job.await())
+        emit(job.await())
     }
 
 }
