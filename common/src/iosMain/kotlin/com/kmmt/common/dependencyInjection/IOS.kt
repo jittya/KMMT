aae9f8d494e7c332a9f73bbe4753acb912b425e2
Jitty Andiyan <jittya@gmail.com>
2022-07-24 14:37:33 +0200

    exclude build files in git

--- common/src/iosMain/kotlin/com/kmmt/common/dependencyInjection/IOS.kt
+++ common/src/iosMain/kotlin/com/kmmt/common/dependencyInjection/IOS.kt
@@ -1,7 +1,9 @@
 package com.kmmt.common.dependencyInjection
 
+import com.kmmt.common.expectations.Application
 import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
 object IOS : KoinComponent {
-
+    val application: Application by inject ()
 }
